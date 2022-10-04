0cdb887a796067cde795626c01feefb7f5e26260
filipiwaniuk <filipiwaniuk@gmail.com>
2022-10-04 18:51:41 +0100

    Fixed the base function to pass the tests via implementation of control block of if with itteration of values by the usage of for iterator

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function (string_input, value_input) {
 
+  let desiredOutput = ``;
+
+  if (value_input >= 1) {
+    for (let x = 0; x < value_input; x++) {
+      desiredOutput += string_input;
+    }
+    return desiredOutput;
+  } else if (value_input < 0){
+    return `ERROR`;
+  } else {
+    return ``;
+  }
 };
+
+console.log(repeatString('hey', -1));
 
 // Do not edit below this line
 module.exports = repeatString;
