de03439aae3401fb83dc69d581e9c5acca24db95
filipiwaniuk <filipiwaniuk@gmail.com>
2022-10-05 19:21:50 +0100

    Added base solution by converting the string into array and passing such in reverse by unshift operator to reverse desired input

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+// TODO TRIM THE STRING PRIOR TO CONVERSION TO THE ARRAY
 
-};
+const reverseString = function (input) {
+
+  const reverseAssist = [];
+  let stringJoin = ``;
+
+  for (let x = 0; x < input.length; x++) {
+
+    reverseAssist.unshift(input[x])
+    stringJoin = reverseAssist.join('');
+  }
+  return stringJoin
+}
+console.log(reverseString(`red`));
 
 // Do not edit below this line
 module.exports = reverseString;
