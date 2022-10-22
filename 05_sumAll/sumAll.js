72c2f8aafc8b790be3d29b022c548c3225a6e84f
filipiwaniuk <filipiwaniuk@gmail.com>
2022-10-22 23:06:43 +0100

    Added base solution which iterates through the range of value between lower and upper range and adds the range to total value

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function (lowerBound, upperBound) {
+
+  const lower = arguments[0];
+  const upper = arguments[1];
+  let total = 0;
+  let counter = 0;
+   
+  if (lower < upper) {
+    for (let x = 0; x < upper; x++) {
+      counter++; 
+      total += counter;
+      console.log(`counter  ---- ${counter}`);
+    }
+  }
+
+  return total;
 
 };
+
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
