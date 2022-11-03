d992723856909899fd5ffea9768bdf8a53f80dc2
filipiwaniuk <filipiwaniuk@gmail.com>
2022-11-03 16:32:05 +0000

    Added base solution, utilizes the Math module to round the conversion to desired point

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  return Math.round((input-32) * .5556);
 };
 
-const ctof = function() {
-
+const ctof = function(input) {
+  return Math.round((input*1.8) + 32);
 };
+
+
+console.log(ftoc(50));
+console.log(ctof(30));
 
 // Do not edit below this line
 module.exports = {
